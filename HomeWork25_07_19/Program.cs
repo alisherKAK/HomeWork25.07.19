dd984c3ad26bbe3b5152979aba563b8e9143ecab
alisherKak <alisher.alish333@gmail.com>
2019-07-25 11:46:29 +0600

    Добавьте файлы проекта.

---
+++ HomeWork25_07_19/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWork25_07_19
+{
+    //Chain of responsibility
+    public interface IProblemSolver
+    {
+        IProblemSolver SetNext(IProblemSolver problemSolver);
+        string Solve(object problem);
+    }
+
+    public abstract class ProblemSolver : IProblemSolver
+    {
+        private IProblemSolver _nextProblemSolver;
+
+        public IProblemSolver SetNext(IProblemSolver problemSolver)
+        {
+            _nextProblemSolver = problemSolver;
+            return problemSolver;
+        }
+
+        public virtual string Solve(object problem)
+        {
+            if (_nextProblemSolver != null)
+            {
+                return _nextProblemSolver.Solve(problem);
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+    }
+
+    public class MathProblemSolver : ProblemSolver
+    {
+        public override string Solve(object problem)
+        {
+            if(problem.ToString().ToLower() == "math")
+            {
+                return "MathProblemSolver is able to solve this problem";
+            }
+            else
+            {
+                return base.Solve(problem);
+            }
+        }
+    }
+
+    public class LinguisticProblemSolver : ProblemSolver
+    {
+        public override string Solve(object problem)
+        {
+            if (problem.ToString().ToLower() == "linguist")
+            {
+                return "LinguisticProblemSolver is able to solve this problem";
+            }
+            else
+            {
+                return base.Solve(problem);
+            }
+        }
+    }
+
+    public class TechnicProblemSolver : ProblemSolver
+    {
+        public override string Solve(object problem)
+        {
+            if (problem.ToString().ToLower() == "technic")
+            {
+                return "TechnicProblemSolver is able to solve this problem";
+            }
+            else
+            {
+                return base.Solve(problem);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathProblemSolver mathProblemSolver = new MathProblemSolver();
+            LinguisticProblemSolver linguisticProblemSolver = new LinguisticProblemSolver();
+            TechnicProblemSolver technicProblemSolver = new TechnicProblemSolver();
+
+            linguisticProblemSolver.SetNext(mathProblemSolver).SetNext(technicProblemSolver);
+
+            List<string> problems = new List<string> { "linguist", "math", "technic" };
+
+            foreach(string problem in problems)
+            {
+                Console.WriteLine(linguisticProblemSolver.Solve(problem));
+            }
+        }
+    }
+}
---
+++ Mediator/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Mediator
+{
+    //Mediator
+    public abstract class Mediator
+    {
+        public abstract void Send(string msg, Colleague colleague);
+    }
+
+    public abstract class Colleague
+    {
+        protected Mediator mediator;
+
+        public Colleague(Mediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public virtual void Send(string message)
+        {
+            mediator.Send(message, this);
+        }
+        public abstract void Notify(string message);
+    }
+
+    public class Client : Colleague
+    {
+        public Client(Mediator mediator) : base(mediator)
+        {}
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("Сообщение клиенту: " + message);
+        }
+    }
+
+    public class Waiter : Colleague
+    {
+        public Waiter(Mediator mediator) : base(mediator)
+        { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("Сообщение официанту: " + message);
+        }
+    }
+
+    public class Cheff : Colleague
+    {
+        public Cheff(Mediator mediator) : base(mediator)
+        { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("Сообщение повару: " + message);
+        }
+    }
+
+    public class ManagerMediator : Mediator
+    {
+        public Client Client;
+        public Waiter Waiter;
+        public Cheff Cheff;
+
+        public override void Send(string msg, Colleague colleague)
+        {
+            if(colleague == Client)
+            {
+                Waiter.Notify(msg);
+            }
+            else if(colleague == Waiter)
+            {
+                Cheff.Notify(msg);
+            }
+            else if(colleague == Cheff)
+            {
+                Client.Notify(msg);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManagerMediator managerMediator = new ManagerMediator();
+            Client client = new Client(managerMediator);
+            Waiter waiter = new Waiter(managerMediator);
+            Cheff cheff = new Cheff(managerMediator);
+
+            managerMediator.Client = client;
+            managerMediator.Cheff = cheff;
+            managerMediator.Waiter = waiter;
+
+            client.Send("Хочу сделать заказ");
+            waiter.Send("Клиент сделал заказ");
+            cheff.Send("Заказ будет выполнен в течений 10 мин");
+        }
+    }
+}
